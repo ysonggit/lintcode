3a84e258f8c4e4d29801652b19be36814b7ddd7d
ysonggit <ysonggit@users.noreply.github.com>
2015-04-13 23:09:53 -0400

    Update kth_prime_number.java

--- kth_prime_number.java
+++ kth_prime_number.java
@@ -2,6 +2,16 @@ class Solution {
     /**
      * @param k: The number k.
      * @return: The kth prime number as description.
+     * Ugly numbers
+     * DP: primes[i] : the i-th minimum prime number 
+     * base primes[0] = 1
+     * set 3 pointers a, b, c, initialize them as 0
+     * primes[i] = min of primes[a] * 3, 
+     *                    primes[b] * 5, 
+     *                    primes[c] * 7
+     * 
+     * key : if primes[i] is from primes[a] , then a++, do similar operations on b, and c
+     * here is the difficulty point: how to code ?
      */
     public long kthPrimeNumber(int k) {
         long primes[] = new long[k+1];
