aa0b311ce01d3d64e935c6be47c94ec991c64a4c
ysonggit <ysonggit@users.noreply.github.com>
2015-04-20 22:17:58 -0400

    Create find_minimum_in_rotated_sorted_arrayII.java

---
+++ find_minimum_in_rotated_sorted_arrayII.java
@@ -0,0 +1,30 @@
+public class Solution {
+    /**
+     * @param num: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    public int findMin(int[] num) {
+        int n = num.length;
+        if(n==0) return 0;
+        int front = 0, back = n-1;
+        while(front<=back){
+            if(front==back) return num[front];// [1]
+            if(back-front==1) return Math.min(num[front], num[back]); //mistake [1,2]
+            int mid = (front+back)/2;
+            if(mid<back && num[mid] > num[mid+1]) return num[mid+1]; // [3 4 1 2]
+            if(mid>front && num[mid] < num[mid-1]) return num[mid];// [4 1 2 3]
+            if(num[mid]>num[back]){
+                // search right half
+                front = mid+1;
+            }else if(num[mid] < num[back]){
+                // search left half
+                back = mid-1;
+            }else{
+                back --; // diff from problem without dup
+            }
+        }
+        return 0;
+    }
+}
+
+
