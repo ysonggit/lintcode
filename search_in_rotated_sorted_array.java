2bf40cb3428f02efef9a36430629a022c2c77902
ysonggit <ysonggit@users.noreply.github.com>
2015-04-22 22:45:24 -0400

    Create search_in_rotated_sorted_array.java

---
+++ search_in_rotated_sorted_array.java
@@ -0,0 +1,50 @@
+public class Solution {
+    /** 
+     *@param A : an integer rotated sorted array
+     *@param target :  an integer to be searched
+     *return : an integer
+     * combine search in sorted array and minium of rotated array
+     * 1. find the position of pivot
+     * 2. binary search two sub arrays divided by this pivot
+     */
+    int findMin(int[] A){
+        int n = A.length;
+        int front =0, back=n-1;
+        while(front<=back){
+            if(front==back) return front;
+            if(back-front==1){
+                int pivot=Math.min(A[front], A[back]);
+                return (pivot==A[front]) ? front : back;
+            }
+            int mid = (front+back)/2;
+            if(mid<back && A[mid]>A[mid+1]) return mid+1;
+            if(mid>front && A[mid-1]>A[mid]) return mid;
+            if(A[mid] >A[back]){
+                front=mid+1;
+            }else{
+                back=mid-1;
+            }
+        }
+        return 0;
+    }
+    
+    int binarySearch(int[] A, int target, int low, int high){
+        if(low>high) return -1;
+        int mid = (low+high)/2;
+        if(A[mid]==target) return mid;
+        if(A[mid]<target) return binarySearch(A, target, mid+1, high);
+        return binarySearch(A, target, low, mid-1);
+    }
+     
+    public int search(int[] A, int target) {
+        int n = A.length;
+        if(n==0) return -1;
+        int pivot_idx = findMin(A);
+        int left_idx = binarySearch(A, target, 0, pivot_idx-1);
+        if(left_idx>=0) return left_idx;
+        int right_idx = binarySearch(A, target, pivot_idx, n-1);
+        if(right_idx>=0) return right_idx;
+        return -1;
+    }
+}
+
