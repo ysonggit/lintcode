ad072f1e8a0852078f289945903d35cdebed1abd
ysonggit <ysonggit@users.noreply.github.com>
2015-05-07 21:17:26 -0400

    topological sort BFS

--- topological_sorting.java
+++ topological_sorting.java
@@ -10,55 +10,38 @@ public class Solution {
     /**
      * @param graph: A list of Directed graph node
      * @return: Any topological order for the given graph.
-     *  L ← Empty list that will contain the sorted elements
-        S ← Set of all nodes with no incoming edges
-        while S is non-empty do
-            remove a node n from S
-            add n to tail of L
-            for each node m with an edge e from n to m do
-                remove edge e from the graph
-                if m has no other incoming edges then
-                    insert m into S
-        if graph has edges then
-            return error (graph has at least one cycle)
-        else 
-            return L (a topologically sorted order)
      */    
     public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
-        HashSet<Integer> children = new HashSet<Integer>();
-        HashMap<Integer, HashSet<Integer>> child_parents = new HashMap<Integer, HashSet<Integer>>();
-        for(DirectedGraphNode node : graph){
-            for(DirectedGraphNode child : node.neighbors){
-                if(!children.contains(child.label)){
-                    children.add(child.label);
-                }
-                if(!child_parents.containsKey(child.label)){
-                    HashSet<Integer> parents = new HashSet<Integer>();
-                    parents.add(node.label);
-                    child_parents.put(child.label, parents);
-                }else{
-                    child_parents.get(child.label).add(node.label);
-                }
+        ArrayList<DirectedGraphNode> toporder = new ArrayList<DirectedGraphNode>();
+        if(graph.size()==0) return toporder;
+        Queue<DirectedGraphNode> nodes_without_inedge = new LinkedList<DirectedGraphNode>();
+        HashMap<Integer, DirectedGraphNode> nodes = new HashMap<Integer, DirectedGraphNode>();
+        // assume node label are from 0 to n
+        int indegree[] = new int [graph.size()];// indegree[i] = j means node.label i has j inedges
+        for(int i=0; i<graph.size(); i++){
+            DirectedGraphNode cur = graph.get(i);
+            nodes.put(cur.label, cur);
+            for(DirectedGraphNode neighbor : cur.neighbors){
+                indegree[neighbor.label]++; 
             }
         }
-        Queue<DirectedGraphNode> src_nodes = new LinkedList<>(); //nodes without incoming edges
-        for(DirectedGraphNode n : graph){
-            if(!children.contains(n.label) ) {
-                src_nodes.add(n);
-            }
+        // put nodes without indegree to queue
+        for(int i=0; i<indegree.length; i++){
+            if(indegree[i]==0) nodes_without_inedge.offer(nodes.get(i));
         }
-        ArrayList<DirectedGraphNode> res = new ArrayList<>();
-        while(!src_nodes.isEmpty()){
-            DirectedGraphNode n = src_nodes.poll(); 
-            res.add(n);
+        while(!nodes_without_inedge.isEmpty()){
+            DirectedGraphNode n = nodes_without_inedge.poll();
+            toporder.add(n);
+            // for each node m with edge e from n->m
+            // remove e from graph 
             for(DirectedGraphNode m : n.neighbors){
-                child_parents.get(m.label).remove(n.label);
-                if(child_parents.get(m.label).isEmpty()){
-                    src_nodes.add(m);
-                }
+                indegree[m.label]--;
+                if(indegree[m.label]==0) nodes_without_inedge.offer(m);
             }
         }
-        return res;
+        return toporder;
     }
 }
+
+
 
