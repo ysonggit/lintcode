be1aec31e564c755c3336b96af5ef9f4cf61d556
ysonggit <ysonggit@users.noreply.github.com>
2015-05-16 23:10:40 -0400

    Create assignment_operator_overloading.cpp

---
+++ assignment_operator_overloading.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    char *m_pData;
+    Solution() {
+        this->m_pData = NULL;
+    }
+    Solution(char *pData) {
+        this->m_pData = pData;
+    }
+    /**
+     * http://guitarpenguin.is-programmer.com/posts/40909.html
+     */
+    // Implement an assignment operator
+    Solution & operator=(const Solution &object) {
+        if(this == & object) return *this;
+        delete[]  m_pData; // release memory before assign new memory space
+        m_pData = NULL;
+        m_pData = new char[strlen(object.m_pData)+1];
+        strcpy(m_pData, object.m_pData);
+        return *this;
+    }
+};
