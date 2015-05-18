017fbcdbb065e91396f93d3c71df3ef0cd0b6b55
ysonggit <ysonggit@users.noreply.github.com>
2015-05-17 20:23:05 -0400

    Update assignment_operator_overloading.cpp

--- assignment_operator_overloading.cpp
+++ assignment_operator_overloading.cpp
@@ -12,6 +12,7 @@ public:
      */
     // Implement an assignment operator
     Solution & operator=(const Solution &object) {
+        if(object.m_pData==NULL) return *this; // mistake : input A = D, D.m_pData = NULL;
         if(this == & object) return *this;
         delete[]  m_pData; // release memory before assign new memory space
         m_pData = NULL;
