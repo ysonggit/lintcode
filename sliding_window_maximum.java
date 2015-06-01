d7388d19a0d02ba53af4ce9745e70231dcb037e8
ysonggit <ysonggit@users.noreply.github.com>
2015-06-01 13:40:55 -0400

    Update sliding_window_maximum.java

--- sliding_window_maximum.java
+++ sliding_window_maximum.java
@@ -1,10 +1,10 @@
-public class Solution {
+/*public class Solution {
     /**
      * @param nums: A list of integers.
      * @return: The maximum number inside the window at each moving.
      * O(nlogk) time O(k) space
      * TLE 
-     */
+     *
 
     class MaxHeapCmp implements Comparator<Integer>{
         public int compare(Integer a, Integer b){
@@ -30,4 +30,40 @@ public class Solution {
         return res;
     }
 }
+*/
 
+public class Solution {
+    /**
+     * @param nums: A list of integers.
+     * @return: The maximum number inside the window at each moving.
+     * O(n) time O(k) space
+     */
+    public ArrayList<Integer> maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if(k==0|| n==0) return res;
+        // deque maintains the maximum number of current window of size k
+        // the first element in deque is the index of the max number in the window
+        // from the second to the end of deque, they are candidates of max elements in window
+        Deque<Integer> q = new LinkedList<Integer>();
+        for(int i=0; i<k; i++){
+            while(!q.isEmpty() && nums[i] > nums[q.getLast()]){
+                q.removeLast();
+            }
+            q.offerLast(i);
+        }
+        res.add(nums[q.getFirst()]);
+        for(int i=k; i<n; i++){
+            while(!q.isEmpty() && i - q.getFirst()>=k){
+                // the max is too old (outside window) should drop element
+                q.removeFirst();
+            }
+            while(!q.isEmpty() && nums[i] > nums[q.getLast()]){
+                q.removeLast();
+            }
+            q.offerLast(i);
+            res.add(nums[q.getFirst()]);
+        }
+        return res;
+    }
+}
