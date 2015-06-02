3ba8eb2fdef01fa3c004f613556e397e1af5a11c
ysonggit <ysonggit@users.noreply.github.com>
2015-06-02 00:19:46 -0400

    Update kth_smallest_number_in_sorted_matrix.java

--- kth_smallest_number_in_sorted_matrix.java
+++ kth_smallest_number_in_sorted_matrix.java
@@ -32,3 +32,85 @@ public class Solution {
        return 0;
     }
 }
+
+public class Solution {
+    /**
+     * @param matrix: a matrix of integers
+     * @param k: an integer
+     * @return: the kth smallest number in the matrix
+     * http://www.geeksforgeeks.org/kth-smallest-element-in-a-row-wise-and-column-wise-sorted-2d-array-set-1/
+     * The idea is to use min heap. Following are detailed step.
+        1) Build a min heap of elements from first row.
+            A heap entry also stores row number and column number.
+        2) Do following k times.
+            a) Get minimum element (or root) from min heap.
+            b) Find row number and column number of the minimum element.
+            c) Replace root with the next element from same column and min-heapify the root.
+        3) Return the last extracted root.
+        [1 ,5 ,7]
+        [3 ,7 ,8]
+        [4 ,8 ,9]
+        1) minheap 
+                1
+               / \
+              5   7
+        2) k=1
+           a) extract root 1
+           b) row 0, col 0
+           c) root = 3
+                    / \
+                   5   7
+           k=2
+           a) extract root 3
+           b) row 1, col 0
+           c) root = 4
+                    / \
+                   5   7
+           k=3
+           a) extract root 4
+           b) row 2, col 0
+           c) root = 5
+                    / \
+                   7   7
+           k=4
+           a) extract root 5
+           b) row 0, col 1
+           c) root 7
+                  / \
+                 7   8
+        3) return 5
+     
+     Memory Limit Exceed! QNMB!!! 
+     
+     */
+    class Entry{
+        int val, row, col;
+        Entry(int v, int r, int c){
+            val = v;
+            row = r;
+            col = c;
+        }
+    }
+    class EntryCmp implements Comparator<Entry>{
+        public int compare (Entry a, Entry b){
+            return a.val - b.val;
+        }
+    }
+    public int kthSmallest(int[][] matrix, int k) {
+        if(k<=0 || matrix.length==0) return 0;
+        int n = matrix[0].length;
+        PriorityQueue<Entry> minheap = new PriorityQueue<Entry>(n, new EntryCmp());
+        for(int i=0; i<matrix[0].length; i++){
+            minheap.offer(new Entry(matrix[0][i], 0, i));  
+        }
+        int popped_val = Integer.MIN_VALUE;
+        for(int i=0; i<k; i++){
+            Entry root = minheap.poll();
+            popped_val = root.val;
+            if(root.row < matrix.length-1){
+                minheap.offer(new Entry(matrix[root.row+1][root.col], root.row+1, root.col));
+            }
+        }
+        return popped_val;
+    }
+}
